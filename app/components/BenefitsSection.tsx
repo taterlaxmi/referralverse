f6a7794ce15b6a693157419baa59655a11fb84f5
Laxmi <taterlaxmi@gmail.com>
2026-02-28 00:13:48 +0530

    Removed unused files

--- app/components/BenefitsSection.tsx
+++ app/components/BenefitsSection.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type Benefit = {
     title?: string;
     description?: string;
--- app/components/ComparisonSection.tsx
+++ app/components/ComparisonSection.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type ComparisonTable = {
   title: string;
   columns: string[];
